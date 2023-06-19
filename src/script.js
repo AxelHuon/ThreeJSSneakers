334d0b8fb9e61b8e617dbacf13acb52aa9e499bb
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-06-19 15:53:13 +0200

    thrid

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
 import * as THREE from "three"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js"
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js"
+import {OBJLoader} from "three/examples/jsm/loaders/OBJLoader.js"
 import GUI from 'lil-gui';
+import { gsap } from 'gsap';
+import {TweenMax} from "gsap/gsap-core";
 
 
 // Scene
@@ -9,21 +11,14 @@ const scene = new THREE.Scene();
 const loader = new OBJLoader();
 
 
-
 const light = new THREE.AmbientLight(0xffffff, 1);
 light.position.set(0, 10, 5);
 scene.add(light);
 
 
-
-
-
-
-
 // Sizes
 let sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+	width: window.innerWidth, height: window.innerHeight
 }
 
 // Camera
@@ -34,19 +29,18 @@ scene.add(camera);
 // Renderer
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: false
+	canvas: canvas, alpha: true
 })
 
 const resize = () => {
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
-    
-    // Mettre à jour le ratio de la caméra
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
-    
-    renderer.setSize(sizes.width, sizes.height);
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+	
+	// Mettre à jour le ratio de la caméra
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+	
+	renderer.setSize(sizes.width, sizes.height);
 }
 
 resize();
@@ -54,112 +48,104 @@ resize();
 window.addEventListener('resize', resize);
 
 
-
-
-
-
 // Controls
-const controls = new OrbitControls( camera, canvas )
-controls.enabled = false
+const controls = new OrbitControls(camera, canvas);
+controls.enabled = false;
+controls.enableDamping = true;
 
 
-var clock = new THREE.Clock();
-
-
-    loader.load(
-        'baked_mesh_modified.obj',
-        function(object) {
-            
-            
-            
-            
-            // Cette fonction sera appelée lorsque le chargement sera terminé
-            // Ajoutez l'objet à votre scène
-            scene.add(object);
-            
-            // Définir l'échelle de l'objet
-            object.scale.x = 9; // Augmenter la taille sur l'axe x
-            object.scale.y = 9; // Augmenter la taille sur l'axe y
-            object.scale.z = 9; // Augmenter la taille sur l'axe z
-            
-            object.position.z = 0;
-            object.position.x = 1;
-            object.rotation.z = -1;
-            object.rotation.y = 84.9;
-            
-            // Variables pour l'animation de rotation
-            let startRotation = object.rotation.clone(); // Point de départ
-            let endRotation = new THREE.Euler(0, 78.5, -1); // Point d'arrivée
-            let elapsedTime = 0; // Temps écoulé depuis le début de l'animation
-            
-            // Fonction de rendu pour l'animation
-            function render() {
-                // Calculer le delta entre deux frames en secondes
-                var delta = clock.getDelta();
-                
-                // Calculer le pourcentage de progression (entre 0 et 1) en fonction du temps écoulé
-                var progress = Math.min(elapsedTime / 3, 1); // 2 secondes
-                
-                // Interpoler la rotation entre le point de départ et d'arrivée
-                var currentRotation = new THREE.Euler().copy(startRotation);
-                currentRotation.x += (endRotation.x - startRotation.x) * progress;
-                currentRotation.y += (endRotation.y - startRotation.y) * progress;
-                currentRotation.z += (endRotation.z - startRotation.z) * progress;
-                
-                // Appliquer la rotation à l'objet
-                object.rotation.copy(currentRotation);
-                
-                // Mettre à jour le temps écoulé
-                elapsedTime += delta;
-                
-                // Appeler la fonction de rendu récursivement
-                requestAnimationFrame(render);
-            }
-            
-            // Appeler la fonction de rendu pour lancer l'animation
-            render();
-        },
-        function(xhr) {
-            // Cette fonction sera appelée pendant le chargement
-            console.log((xhr.loaded / xhr.total * 100) + '% chargé');
-        },
-        function(error) {
-            // Cette fonction sera appelée s'il y a une erreur de chargement
-            console.error('Erreur de chargement', error);
-        }
-    );
-
-
-
+// Enable controls after a delay
+setTimeout(() => {
+	controls.enabled = true;
+}, 3000); // Enable controls after 3 seconds
 
 
 // Création d'un TextureLoader
 const textureLoader = new THREE.TextureLoader();
 
-// Chargement de la texture diffuse
-textureLoader.load('baked_mesh_tex0.png', function (diffuseTexture) {
-    // Chargement de la texture d'occlusion ambiante (aoMap)
-    textureLoader.load('baked_mesh_ao0.png', function (aoTexture) {
-        // Chargement de la texture normale
-        textureLoader.load('baked_mesh_norm0.png', function (normalTexture) {
-            // Appliquer les textures à votre objet OBJ
-            scene.traverse(function (child) {
-                if (child instanceof THREE.Mesh) {
-                    child.material.map = diffuseTexture;
-                    child.material.aoMap = aoTexture;
-                    child.material.normalMap = normalTexture;
-                    child.material.needsUpdate = true; // Assurez-vous que la mise à jour du matériau est activée
-                }
-            });
-        }, undefined, function (error) {
-            console.error('Erreur de chargement de la texture normale', error);
-        });
-    }, undefined, function (error) {
-        console.error('Erreur de chargement de la texture d\'occlusion ambiante', error);
-    });
-}, undefined, function (error) {
-    console.error('Erreur de chargement de la texture diffuse', error);
+
+
+
+loader.load('baked_mesh_modified.obj', function (object) {
+	
+	
+	// Chargement de la texture diffuse
+	textureLoader.load('baked_mesh_tex0.png', function (diffuseTexture) {
+		// Chargement de la texture d'occlusion ambiante (aoMap)
+		textureLoader.load('baked_mesh_ao0.png', function (aoTexture) {
+			// Chargement de la texture normale
+			textureLoader.load('baked_mesh_norm0.png', function (normalTexture) {
+				// Appliquer les textures à votre objet OBJ
+				scene.traverse(function (child) {
+					if (child instanceof THREE.Mesh) {
+						child.material.map = diffuseTexture;
+						child.material.aoMap = aoTexture;
+						child.material.normalMap = normalTexture;
+						child.material.needsUpdate = true; // Assurez-vous que la mise à jour du matériau est activée
+					}
+				});
+			}, undefined, function (error) {
+				console.error('Erreur de chargement de la texture normale', error);
+			});
+		}, undefined, function (error) {
+			console.error('Erreur de chargement de la texture d\'occlusion ambiante', error);
+		});
+	}, undefined, function (error) {
+		console.error('Erreur de chargement de la texture diffuse', error);
+	});
+	
+	
+
+	scene.add(object);
+	// Définir l'échelle de l'objet
+	object.scale.x = 9; // Augmenter la taille sur l'axe x
+	object.scale.y = 9; // Augmenter la taille sur l'axe y
+	object.scale.z = 9; // Augmenter la taille sur l'axe z
+	object.name = 'DarkMocha';
+	
+	object.position.z = 0;
+	object.position.x = 4.5;
+	object.rotation.z = -1;
+	object.rotation.y = -6;
+	const gui = new GUI()
+	const cubeFolder = gui.addFolder('Sneakers')
+	cubeFolder.add(object.position, 'x', 0, Math.PI * 2)
+	cubeFolder.add(object.rotation, 'x', 0, Math.PI * 2)
+	cubeFolder.add(object.rotation, 'y', 0, Math.PI * 2)
+	cubeFolder.add(object.rotation, 'z', 0, Math.PI * 2)
+	cubeFolder.open()
+	
+	
+	var objectToAnimate = scene.getObjectByName('DarkMocha');
+	
+	if (objectToAnimate) {
+		// Animation de l'objet
+		function animateObject() {
+			// Utiliser GSAP pour animer la rotation sur l'axe des y de 1 à 3 sur une durée de 2 secondes
+			TweenMax.to(objectToAnimate.rotation, 3, { y: 3.61});
+			TweenMax.to(objectToAnimate.position, 3, { x: 0});
+			// Appeler la fonction de rendu récursivement
+			requestAnimationFrame(animateObject);
+		}
+		
+		// Lancer l'animation de l'objet
+		animateObject();
+	}
+	
+	
+	
+	
+}, function (xhr) {
+	// Cette fonction sera appelée pendant le chargement
+	console.log((xhr.loaded / xhr.total * 100) + '% chargé');
+}, function (error) {
+	// Cette fonction sera appelée s'il y a une erreur de chargement
+	console.error('Erreur de chargement', error);
 });
+
+
+
+
 
 
 
@@ -168,9 +154,9 @@ textureLoader.load('baked_mesh_tex0.png', function (diffuseTexture) {
 
 // Animate
 const animate = () => {
-    controls.update();
-    renderer.render(scene, camera);
-    window.requestAnimationFrame(animate);
+	controls.update();
+	renderer.render(scene, camera);
+	window.requestAnimationFrame(animate);
 };
 
 animate()
